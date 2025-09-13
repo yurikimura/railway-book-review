01542f42c170e0495c28194fe84b17c5f1fcedba
Yuri Kimura <yurikimura@YurinoMacBook-Pro.local>
2025-09-13 19:01:57 +0900

    vite.configを修正

--- vite.config.ts
+++ vite.config.ts
@@ -11,18 +11,6 @@ export default defineConfig({
         target: 'https://railway.bookreview.techtrain.dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
       },
     },
   },
